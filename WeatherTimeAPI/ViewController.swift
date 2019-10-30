131aaba990b7e94adb1b8db1e602d8455a98e47a
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-10-30 13:12:01 -0400

    Initial Commit

---
+++ WeatherTimeAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTimeAPI
+//
+//  Created by Uppalapati SivaPrrasad on 30/10/19.
+//  Copyright © 2019 Uppalapati SivaPrrasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
